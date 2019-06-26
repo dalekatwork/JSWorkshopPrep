f7305c923169e2a13198a9c6c5725dadf54d57d7
Pooja Maheshwari <pooja.maheshwari247@gmail.com>
2019-06-26 12:46:55 +0200

    added cat dancing

--- exercise.js
+++ exercise.js
@@ -1,7 +1,7 @@
 // LinkedIn connection requests tend to pile up
 // To accept all linkedIn invites using a loop
 
-var allInvites = document.getElementsByClassName('artdeco-button--secondary');
+var allInvites = document.getElementsByClassName('invitation-card__action-btn artdeco-button artdeco-button--secondary ');
 for (var i = 0; i < allInvites.length; i++) {
     i.click();
 }
